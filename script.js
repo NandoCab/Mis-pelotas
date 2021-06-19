1ef2227f73ca1464650839b642ffda19d1866bec
gthbskate <skate.boarding@live.com.mx>
2021-06-19 15:10:48 -0500

    Finalizado

--- script.js
+++ script.js
@@ -46,6 +46,20 @@ Ball.prototype.update = function() {
     this.y += this.velY;
 }
 
+Ball.prototype.collisionDetect = function() {
+    for (let j = 0; j < balls.length; j++) {
+        if (!(this === balls[j])) {
+            const dx = this.x - balls[j].x;
+            const dy = this.y - balls[j].y;
+            const distance = Math.sqrt(dx + dx + dy + dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].color = this.color = "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0,255) + ")";
+            }
+        }
+    }
+}
+
 let balls = [];
 while (balls.length < 25) {
     let size = random(10,20);
@@ -67,6 +81,7 @@ function loop() {
     for (let i = 0; i < balls.length; i++) {
         balls[i].draw();
         balls[i].update();
+        balls[i].collisionDetect();
     }
 
     requestAnimationFrame(loop);
